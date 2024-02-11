dad2597dbc105366a20b2479ed8898c81f52473b
VivekDeyVD <vivekvd35@gmail.com>
2024-02-11 18:09:07 +0530

    java script basic 2

--- javascript/index.js
+++ javascript/index.js
@@ -8,3 +8,32 @@ let b=5;
 console.log(b);
 --b;
 console.log(b);
+
+let marks =80;
+
+if(marks>=90){
+    console.log('A');
+}
+else if(marks>=80 )
+{
+    console.log('B');
+
+}
+else if(marks>=70){
+    console.log('C');
+}
+
+// for loop
+
+for(let i=0; i<=5 ; i++){
+    console.log(i  + 'vivek');
+}
+
+// do while
+
+let i=1;
+
+do{
+    console.log(i);
+    i++;
+}while(i<=10);
---
+++ javascript/index2.js
@@ -0,0 +1,25 @@
+// // console.log('chaliye suru karte hai');
+// // let rectangle ={
+// //     length:1,
+// //     breadth:2,
+
+// //     draw:function(){
+// //         console.log('draw');
+// //     }
+// // };
+
+// function createRectangle (){
+//     return rectangle={
+//         length:1,
+//         breadth:2,
+    
+//         draw:function(){
+//             console.log('draw');
+//         }
+
+//     };
+
+
+
+// }
+// let rectangleObj=rectangle();
